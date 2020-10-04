ef26cbd4a1eb5345d7e94aa74caac6c875d83207
functionalperez <perez.cs@pm.me>
2020-10-04 01:24:34 -0600

    Fixed truncation of values + added --print-command option

--- main.cpp
+++ main.cpp
@@ -2,18 +2,15 @@
 #include <chrono>
 #include <cstdlib>
 #include <filesystem>
-#include <fmt/chrono.h>
 #include <fmt/core.h>
 #include <fmt/ranges.h>
 #include <fstream>
 #include <iostream>
 #include <numeric>
 #include <string>
-#include <tuplet/tuplet.hpp>
 #include <vector>
 
 namespace fs = std::filesystem;
-namespace tb {
 using hrc = std::chrono::high_resolution_clock;
 using seconds_t = std::chrono::duration<double, std::ratio<1, 1>>;
 
@@ -21,7 +18,7 @@ std::string_view std_tuple_code = R"(
 #include <tuple>
 
 int my_func() {
-    auto tup = std::tuple VALUES;
+    auto tup = std::tuple { VALUES };
     auto sum = [](auto... values) { return (values + ...); };
     return std::apply(sum, tup);
 }
@@ -31,7 +28,7 @@ std::string_view tuplet_tuple_code = R"(
 #include <tuplet/tuplet.hpp>
 
 int my_func() {
-    auto tup = tuplet::tuple VALUES;
+    auto tup = tuplet::tuple { VALUES };
     auto sum = [](auto... values) { return (values + ...); };
     return tuplet::apply(sum, tup);
 }
@@ -47,6 +44,7 @@ constexpr tag<2> include_dir;
 constexpr tag<3> bench_file;
 constexpr tag<4> output_files;
 constexpr tag<5> repetitions;
+constexpr tag<6> print_command;
 } // namespace tags
 auto get_parser() {
     using namespace arglet;
@@ -54,6 +52,7 @@ auto get_parser() {
     return sequence {
         ignore_arg,
         group {
+            flag {tags::print_command, "--print-command"},
             value_flag {tags::repetitions, "--repetitions", 1},
             value_flag {tags::output_files, 'o', std::vector<fs::path>()},
             prefixed_value {tags::include_dir, 'I', "tuplet/include"sv},
@@ -85,58 +84,54 @@ void write(fs::path const& p, std::string_view contents) {
 
     file.write(contents.data(), contents.size());
 }
-} // namespace tb
-int main(int argc, char const** argv) {
-    using namespace tb;
+int main(int argc, char const** argv) try {
+
     auto parser = get_parser();
     parser.parse(argc, argv);
-    try {
-        auto& bench_file = parser[tags::bench_file];
+    auto& bench_file = parser[tags::bench_file];
 
-        prepare_path(bench_file);
+    prepare_path(bench_file);
 
-        switch (parser[tags::library]) {
-            case lib::stdlib: write(bench_file, std_tuple_code); break;
-            case lib::tuplet: write(bench_file, tuplet_tuple_code); break;
-        }
+    switch (parser[tags::library]) {
+        case lib::stdlib: write(bench_file, std_tuple_code); break;
+        case lib::tuplet: write(bench_file, tuplet_tuple_code); break;
+    }
 
-        std::vector<std::fstream> outputs;
-        for (auto& p : parser[tags::output_files]) {
-            outputs.emplace_back(
-                p.string(), std::ios_base::trunc | std::ios_base::out);
-        }
+    std::vector<std::fstream> outputs;
+    for (auto& p : parser[tags::output_files]) {
+        outputs.emplace_back(
+            p.string(), std::ios_base::trunc | std::ios_base::out);
+    }
 
-        int reps = parser[tags::repetitions];
-        for (auto size : parser[tags::sizes]) {
-            auto values = std::vector<size_t>(size);
-            std::iota(values.begin(), values.end(), size_t(0));
-            auto command = fmt::format(
-                "g++-10 -std=c++20 -x c++ -c -I{} -DVALUES='{}' {}",
-                parser[tags::include_dir],
-                values,
-                bench_file.string());
+    int reps = parser[tags::repetitions];
+    for (auto size : parser[tags::sizes]) {
+        auto values = std::vector<size_t>(size);
+        std::iota(values.begin(), values.end(), size_t(0));
+        auto command = fmt::format(
+            "g++-10 -std=c++20 -x c++ -c -I{0} -DVALUES='{1}' {2} -o {2}.o",
+            parser[tags::include_dir],
+            fmt::join(values, ", "),
+            bench_file.string());
 
-            for (int i = 0; i < reps; i++) {
-                double time = 0.0;
-                {
-                    asm volatile("");
-                    auto t0 = hrc::now();
-                    system(command.c_str());
-                    time =
-                        std::chrono::duration_cast<seconds_t>(hrc::now() - t0)
-                            .count();
-                    asm volatile("");
-                }
+        if (parser[tags::print_command])
+            fmt::print("Command: {}\n", command);
 
-                auto line = fmt::format("{}, {}\n", size, time);
+        for (int i = 0; i < reps; i++) {
+            asm volatile("");
+            auto t0 = hrc::now();
+            system(command.c_str());
+            double time =
+                std::chrono::duration_cast<seconds_t>(hrc::now() - t0).count();
+            asm volatile("");
 
-                std::cout << line;
-                for (auto& dest : outputs) {
-                    dest << line;
-                }
+            auto line = fmt::format("{}, {}\n", size, time);
+
+            std::cout << line;
+            for (auto& dest : outputs) {
+                dest << line;
             }
         }
-    } catch (std::exception& ex) {
-        fmt::print(stderr, "Error: {}", ex.what());
     }
+} catch (std::exception& ex) {
+    fmt::print(stderr, "Error: {}", ex.what());
 }
